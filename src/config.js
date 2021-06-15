c28b7077fc4b681627b699bc52770dd5f8bdbf9c
Thomas Kelly <thomask540@gmail.com>
2021-06-15 13:40:00 +0100

    commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// All proxy urls are listed here
+var config = {
+    urls: [
+        "http://webcalc-proxy.40183237.qpc.hal.davecutting.uk/",
+        "http://webcalc-proxy1.40183237.qpc.hal.davecutting.uk/",
+        "http://webcalc-proxy2.40183237.qpc.hal.davecutting.uk/",
+        "http://webcalc-proxy3.40183237.qpc.hal.davecutting.uk/"
+    ],
+    database_url: ["http://webcalc-database.40183237.qpc.hal.davecutting.uk/"]
+};
